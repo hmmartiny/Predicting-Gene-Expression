d1b3e273bc3e708c8c2645417fbd7fde5ea34987
hmmartiny <hanmar@food.dtu.dk>
2020-10-30 14:45:09 +0100

    Made train.py able to train with user given input files

--- src/train.py
+++ src/train.py
@@ -10,12 +10,49 @@ import sys
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_validate
 from sklearn.metrics import roc_auc_score
+import argparse
 
 sys.path.append('..')
 from model.baselines import CreateBaselines
 
-def _load():
-    source = open('data/expr_unirep_solubility_mar24.pkl', 'rb')
+def parse_args():
+    parser = argparse.ArgumentParser(description = "Train ML classifiers")
+
+    parser.add_argument(
+        '-s', '--source',
+        type=str,
+        required=True,
+        help='Pickled UniRep formatted sequences',
+        dest='unirep_source'
+    )
+
+    parser.add_argument(
+        '-ft', '--freq-train',
+        type=str,
+        required=True,
+        help='File with training data for AAFreq models',
+        dest='freq_train'
+    )
+    parser.add_argument(
+        '-fv', '--freq-valid',
+        type=str,
+        required=True,
+        help='File with validation data for AAFreq models',
+        dest='freq_valid'
+    )
+
+    parser.add_argument(
+        '-o', '--out',
+        type=str,
+        default='data/seed_training',
+        help='Path to store pickled models in',
+        dest='out_path'
+    )
+    
+    return parser.parse_args()
+
+def _load(source):
+    source = open(source)
 
     X_train = pickle.load(source)
     X_valid = pickle.load(source)
@@ -25,15 +62,22 @@ def _load():
     source.close()
     return X_train, X_valid, Y_train, Y_valid
 
-def _load_enz():
-    train = pd.read_hdf('data/enz_dfs.h5', 'train')
-    valid = pd.read_hdf('data/enz_dfs.h5', 'valid')
-    test = pd.read_hdf('data/enz_dfs.h5', 'test')
+def _load_enz(args):
+    train = pd.read_hdf(args.enz, 'train')
+    valid = pd.read_hdf(args.enz, 'valid')
+    test = pd.read_hdf(args.enz, 'test')
 
-    enz_data = pd.read_excel('data/data_combined_cleaned_hhm.xlsx', sheet_name='combined_fixed')
+    enz_data = pd.read_excel(args.enz_overview, sheet_name='combined_fixed')
     enz_data.rename(columns={'Family': 'Enzyme Family', 'Category3': 'Enzyme'}, inplace=True)
 
     return train, valid, test, enz_data
+
+def _load_freq(train, valid):
+
+    train = pd.read_csv(train, index_col=0)
+    valid = pd.read_csv(valid, index_col=0)
+
+    return train, valid
 
 def save_pickle(data, dest):
     with open(dest, 'wb') as d:
@@ -71,10 +115,10 @@ def train_baseline(X_train, X_valid, Y_train, Y_valid, input_cols, out_file=None
     
     return model, train_auc, valid_auc  
 
-def test_enz_seeds(col, n, out_file, model_name):
+def test_enz_seeds(col, n, out_file, model_name, args):
 
     results = {}
-    train, valid, _, enz_data = _load_enz()
+    train, valid, _, enz_data = _load_enz(args)
     y_col = 'is_expression_successful'
 
     input_cols = pd.DataFrame(train['unirep'].values.tolist()).columns.tolist()
@@ -116,14 +160,13 @@ def test_enz_seeds(col, n, out_file, model_name):
 
             #print("Enzyme={}, i={}: Train AUC: {:.3f}, valid AUC: {:.3f}".format(enzyme, i+1, train_auc, valid_auc))
     
-        save_pickle(results, out_file)
+        save_pickle(results, out_file)  
 
-def test_freq_seeds(col, n, out_file, model_name):
+
+def test_freq_seeds(col, n, out_file, model_name, args):
 
     # load data
-    train = pd.read_csv('data/Copy of train3.csv', index_col=0)
-    valid = pd.read_csv('data/Copy of valid3.csv', index_col=0)
-
+    train, valid = _load_freq(train = args.freq_train, valid=args.freq_valid)
     y_col = 'is_expression_successful'
 
     # get cols
@@ -170,61 +213,9 @@ def test_freq_seeds(col, n, out_file, model_name):
         pass   
 
 
-def test_freq_seeds(col, n, out_file, model_name):
-
-    # load data
-    train = pd.read_csv('data/Copy of train3.csv', index_col=0)
-    valid = pd.read_csv('data/Copy of valid3.csv', index_col=0)
-
-    y_col = 'is_expression_successful'
-
-    # get cols
-    valid_aas = 'ARNDCQEGHILKMFPSTWYV'
-    cols = [col + aa for aa in valid_aas]
-
-    X_train = train[cols].copy()
-    X_valid = valid[cols].copy()
-
-    Y_train, Y_valid = train[y_col], valid[y_col]
-    results = {}
-    for i in range(n):
-        random.seed()
-
-        model, train_auc, valid_auc = train_baseline(
-            X_train = X_train,
-            X_valid = X_valid,
-            Y_train = Y_train,
-            Y_valid = Y_valid,
-            input_cols=cols,
-            model_name=model_name
-        )
-
-        results[i] = {
-            'model': model,
-            'train_auc': train_auc,
-            'valid_auc': valid_auc
-        }
-
-        print("i={}: Train AUC: {:.3f}, valid AUC: {:.3f}".format(i+1, train_auc, valid_auc))
+def test_seeds(n, input_cols, out_file, model_name, source):
     
-    save_pickle(results, out_file)
-
-    try:
-        train_aucs = np.array([v['train_auc'] for v in results.values()])
-        valid_aucs = np.array([v['valid_auc'] for v in results.values()])
-
-        train_avg_auc, train_std_auc = np.mean(train_aucs), np.std(train_aucs)
-        valid_avg_auc, valid_std_auc = np.mean(valid_aucs), np.std(valid_aucs)
-
-        print("Train AUC: {:.3f} (+/- {:.3f})".format(train_avg_auc, train_std_auc))
-        print("Valid AUC: {:.3f} (+/- {:.3f})".format(valid_avg_auc, valid_std_auc))
-    except:
-        pass   
-
-
-def test_seeds(n, input_cols, out_file, model_name):
-    
-    X_train, X_valid, Y_train, Y_valid = _load()
+    X_train, X_valid, Y_train, Y_valid = _load(source)
 
     results = {}
     results['input_cols'] = input_cols
@@ -264,6 +255,8 @@ def test_seeds(n, input_cols, out_file, model_name):
         pass    
 
 if __name__ == "__main__":
+    args = parse_args()
+
     y_col = 'is_expression_successful'
     unit_cols = np.arange(0, 1900, 1)
 
@@ -272,8 +265,6 @@ if __name__ == "__main__":
         'isoelectric point', 'gravy', 'flexibility',
         'solubility', 'helix', 'turn', 'sheet'
     ] 
-
-    featureImps = pd.read_csv('UniRep_features_imp.csv')
 
     models = {
         'AAFreqSVM': 'aa_',
@@ -285,7 +276,7 @@ if __name__ == "__main__":
 
     }
 
-    path = os.path.realpath(os.path.join('data', 'seed_training'))
+    path = os.path.realpath(args.out_path)
     os.makedirs(path, exist_ok=True)
     for k in sorted(models, key=lambda k: len(models[k]), reverse=False):
         out_file = os.path.join(path, k + '.pkl')
@@ -300,9 +291,7 @@ if __name__ == "__main__":
 
         print(k, ':', model)
 
-        if 'Enzyme' in k:
-            test_enz_seeds(col=input_cols, n=10, out_file=out_file, model_name=model)
-        elif 'AAFreq' in k:
-            test_freq_seeds(col=input_cols, n=10, out_file=out_file, model_name=model)
+        if 'AAFreq' in k:
+            test_freq_seeds(col=input_cols, n=10, out_file=out_file, model_name=model, args=args)
         else:
-            test_seeds(n=10, input_cols=input_cols, out_file=out_file, model_name=model)
+            test_seeds(n=10, input_cols=input_cols, out_file=out_file, model_name=model, source=args.unirep_source)
